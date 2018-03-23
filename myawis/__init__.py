edaee33f5add84d7b9b7aec02aaea56d3d9d3391
Hendi Lie <lie.hendi@gmail.com>
2018-03-24 01:45:00 +1000

    Implemented V4 signing process. Refactored some of the CallAwis function parameters.

--- myawis/__init__.py
+++ myawis/__init__.py
@@ -1,11 +1,11 @@
 import datetime
-import hmac
-import pytz
 import hashlib
-import base64
-import requests
+import hmac
+
+import requests  # pip install requests
 import xmltodict
 from bs4 import BeautifulSoup
+
 try:
     from urllib import quote, urlencode
 except ImportError:
@@ -16,11 +16,8 @@ URLINFO_RESPONSE_GROUPS = ",".join(
      "UsageStats", "Speed", "Language", "OwnedDomains", "LinksInCount",
      "SiteData", "AdultContent"])
 
-
-def create_timestamp():
-    now = datetime.datetime.now(pytz.timezone("GMT"))
-    timestamp = now.isoformat()
-    return timestamp
+TRAFFICINFO_RESPONSE_GROUPS = "History"
+CATEGORYBROWSE_RESPONSE_GROUPS = ",".join(["Categories", "RelatedCategories", "LanguageCategories", "LetterBars"])
 
 
 def is_string(obj):
@@ -29,92 +26,125 @@ def is_string(obj):
     except NameError:
         return isinstance(obj, str)  # python 3
 
-
 class CallAwis(object):
-
-    def __init__(self, domainname, responsegroup, access_id, secret_access_key):
-        self.domainname = domainname
-        self.responsegroup = responsegroup
+    def __init__(self, access_id, secret_access_key):
         self.access_id = access_id
         self.secret_access_key = secret_access_key
-        self.SignatureVersion = "2"
-        self.SignatureMethod = "HmacSHA256"
-        self.ServiceHost = "awis.amazonaws.com"
-        self.range = "31"
-        self.PATH = "/"
 
-    def create_uri(self, params):
-        params = [(key, params[key])
-                  for key in sorted(params.keys())]
-        return urlencode(params)
+    def create_v4_signature(self, request_params):
+        '''
+        Create URI and signature headers based on AWS V4 signing process.
+        Refer to https://docs.aws.amazon.com/AlexaWebInfoService/latest/ApiReferenceArticle.html for request params.
+        :param request_params: dictionary of request parameters
+        :return: URL and header to be passed to requests.get
+        '''
 
-    def create_signature(self):
-        Uri = self.create_uri(self.params)
-        msg = "\n".join(["GET", self.ServiceHost, self.PATH, Uri])
-        try:
-            hmac_signature = hmac.new(
-                self.secret_access_key, msg, hashlib.sha256)
-        except TypeError:
-            hmac_signature = hmac.new(self.secret_access_key.encode(
-                'utf-8'), msg.encode('utf-8'), hashlib.sha256)
-        signature = base64.b64encode(hmac_signature.digest())
-        return quote(signature)
+        method = 'GET'
+        service = 'awis'
+        host = 'awis.us-west-1.amazonaws.com'
+        region = 'us-west-1'
+        endpoint = 'https://awis.amazonaws.com/api'
+        request_parameters = urlencode([(key, request_params[key]) for key in sorted(request_params.keys())])
 
-    def urlinfo(self):
-            # Query Options  # refer to AWIS API reference for full details.
-            # Action =
-        self.params = {
+        # Key derivation functions. See:
+        # http://docs.aws.amazon.com/general/latest/gr/signature-v4-examples.html#signature-v4-examples-python
+        def sign(key, msg):
+            return hmac.new(key, msg.encode('utf-8'), hashlib.sha256).digest()
+
+        def getSignatureKey(key, dateStamp, regionName, serviceName):
+            kDate = sign(('AWS4' + key).encode('utf-8'), dateStamp)
+            kRegion = sign(kDate, regionName)
+            kService = sign(kRegion, serviceName)
+            kSigning = sign(kService, 'aws4_request')
+            return kSigning
+
+        # Create a date for headers and the credential string
+        t = datetime.datetime.utcnow()
+        amzdate = t.strftime('%Y%m%dT%H%M%SZ')
+        datestamp = t.strftime('%Y%m%d') # Date w/o time, used in credential scope
+
+        # Create canonical request
+        canonical_uri = '/api'
+        canonical_querystring = request_parameters
+        canonical_headers = 'host:' + host + '\n' + 'x-amz-date:' + amzdate + '\n'
+        signed_headers = 'host;x-amz-date'
+        payload_hash = hashlib.sha256(''.encode('utf8')).hexdigest()
+        canonical_request = method + '\n' + canonical_uri + '\n' + canonical_querystring + '\n' + canonical_headers + '\n' + signed_headers + '\n' + payload_hash
+
+        # Create string to sign
+        algorithm = 'AWS4-HMAC-SHA256'
+        credential_scope = datestamp + '/' + region + '/' + service + '/' + 'aws4_request'
+        string_to_sign = algorithm + '\n' +  amzdate + '\n' +  credential_scope + '\n' +  hashlib.sha256(canonical_request.encode('utf8')).hexdigest()
+
+        # Calculate signature
+        signing_key = getSignatureKey(self.secret_access_key, datestamp, region, service)
+
+        # Sign the string_to_sign using the signing_key
+        signature = hmac.new(signing_key, (string_to_sign).encode('utf-8'), hashlib.sha256).hexdigest()
+
+        # Add signing information to the request
+        authorization_header = algorithm + ' ' + 'Credential=' + self.access_id + '/' + credential_scope + ', ' +  'SignedHeaders=' + signed_headers + ', ' + 'Signature=' + signature
+        headers = {'X-Amz-Date':amzdate, 'Authorization':authorization_header, 'Content-Type': 'application/xml', 'Accept': 'application/xml'}
+
+        # Create request url
+        request_url = endpoint + '?' + canonical_querystring
+
+        return request_url, headers
+
+    def urlinfo(self, domain, response_group = URLINFO_RESPONSE_GROUPS):
+        '''
+        Provide information about supplied domain as specified by the response group
+        :param domain: Any valid URL
+        :param response_group: Any valid urlinfo response group
+        :return: Traffic and/or content data of the domain in XML format
+        '''
+        params = {
             'Action': "UrlInfo",
-            'Url': self.domainname,
-            'ResponseGroup': self.responsegroup,
-            'SignatureVersion': self.SignatureVersion,
-            'SignatureMethod': self.SignatureMethod,
-            'Timestamp': create_timestamp(),
-            'AWSAccessKeyId': self.access_id,
+            'Url': domain,
+            'ResponseGroup': response_group
         }
 
-        uri = self.create_uri(self.params)
-        signature = self.create_signature()
+        url, headers = self.create_v4_signature(params)
+        return self.return_output(url, headers)
 
-        url = "http://%s/?%s&Signature=%s" % (self.ServiceHost, uri, signature)
-        return self.return_output(url)
-
-    def traffichistory(self, myrange=31, start=20070801):
-        # Action="TrafficHistory"
-        self.params = {
+    def traffichistory(self, domain, response_group=TRAFFICINFO_RESPONSE_GROUPS, myrange=31, start=20070801):
+        '''
+        Provide traffic history of supplied domain
+        :param domain: Any valid URL
+        :param response_group: Any valid traffic history response group
+        :return: Traffic and/or content data of the domain in XML format
+        '''
+        params = {
             'Action': "TrafficHistory",
-            'AWSAccessKeyId': self.access_id,
-            'SignatureMethod': self.SignatureMethod,
-            'SignatureVersion': self.SignatureVersion,
-            'Timestamp': create_timestamp(),
-            'Url': self.domainname,
-            'ResponseGroup': self.responsegroup,
+            'Url': domain,
+            'ResponseGroup': response_group,
             'Range': myrange,
             'Start': start,
         }
-        uri = self.create_uri(self.params)
-        signature = self.create_signature()
-        url = "http://%s/?%s&Signature=%s" % (self.ServiceHost, uri, signature)
-        return self.return_output(url)
 
-    def cat_browse(self, path):
-        # Action=''
-        self.params = {
+        url, headers = self.create_v4_signature(params)
+        return self.return_output(url, headers)
+
+    def cat_browse(self, domain, path, response_group=CATEGORYBROWSE_RESPONSE_GROUPS, descriptions='True'):
+        '''
+        Provide category browse information of specified domain
+        :param domain: Any valid URL
+        :param path: Valid category path
+        :param response_group: Any valid traffic history response group
+        :return: Traffic and/or content data of the domain in XML format
+        '''
+        params = {
             'Action': "CategoryListings",
-            'AWSAccessKeyId': self.access_id,
-            'SignatureMethod': self.SignatureMethod,
-            'SignatureVersion': self.SignatureVersion,
-            'Timestamp': create_timestamp(),
             'ResponseGroup': 'Listings',
             'Path': quote(path),
+            'Descriptions': descriptions
         }
-        uri = self.create_uri(self.params)
-        signature = self.create_signature()
-        url = "http://%s/?%s&Signature=%s" % (self.ServiceHost, uri, signature)
-        return self.return_output(url)
 
-    def return_output(self, url):
-        r = requests.get(url)
+        url, headers = self.create_v4_signature(params)
+        return self.return_output(url, headers)
+
+    def return_output(self, url, headers):
+        r = requests.get(url, headers=headers)
         soup = BeautifulSoup(r.text.encode('utf-8'), 'xml')
         return soup
 
@@ -162,5 +192,5 @@ def flatten_urlinfo(urlinfo, shorter_keys=True):
     _result = {}
     info = xmltodict.parse(str(urlinfo))
     flatten(info["aws:UrlInfoResponse"]["Response"]["UrlInfoResult"]["Alexa"])
-    _result["OutputTimestamp"] = create_timestamp()
+    _result["OutputTimestamp"] = datetime.datetime.utcnow().strftime('%Y%m%dT%H%M%SZ')
     return _result
